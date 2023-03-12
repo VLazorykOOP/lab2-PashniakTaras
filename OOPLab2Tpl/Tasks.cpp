1b55e30c257e0c83f64b39bcefe67feb9b2d58ec
Taras Pashniak <taraspashnyak93@gmail.com>
2023-03-12 21:05:46 +0200

    Тест Пр4

--- OOPLab2Tpl/Tasks.cpp
+++ OOPLab2Tpl/Tasks.cpp
@@ -13,35 +13,6 @@ void MenuTask()
     cout << "    6.  Exit \n";
 }
 
-void Shifruvanna(char S[64], unsigned short Rez[64])
-{
-    unsigned char c;
-    unsigned short r, t, i, b;
-    short j;
-    cin.get(S, 64);
-    for (i = 0; i < 64; i++) //
-    {
-        r = 0; // 0000 0000 0000 0000
-        c = S[i]; // s - 0x73 = 0111 0011
-        t = c;
-        r |= t >> 4; // 0000 0000 0000 0111
-        r |= i << 4; // 0000 0000 0101 0111 if i=5 -> 0000 0000 0000 0101
-        t = c;
-        r |= t << 12; // 0011 0000 0101 0111 if i=5 0000 0000 0000 0101
-        t = 1;
-        b = 0;
-        for (j = 0; j < 16; j++) // обчислення біта парності
-        {
-            if (r & t) {
-                if (b == 0) b = 1; else b = 0;
-            }
-            t <<= 1;
-        }
-        r |= b << 11; // 0011 0000 0101 0111 if i=5 0000 0000 0000 0101
-        Rez[i] = r;
-    }
-}
-
 void task1() {
     // Обчислення виразів з використанням побітових операцій
     // Calculation of expressions using bitwise operations 
@@ -101,7 +72,14 @@ void task3()
 {
     // Шифрування даних з використання стуктур з бітовими полями 
     // Data encryption using structures with bit fields
-    cout << "  Data encryption using structures with bit fields \n";
+    cout << "Data encryption using structures with bit fields \n";
+    int h, x, mb1 = 1, mb3 = 1 << 2;
+    cout << "Input h: ";
+    cin >> h;
+    if ((h & mb1) == 1 && ((h & mb3) >> 2) == 0) x = h << 2;
+    else x = h >> 1;
+    cout << "h= " << h << "; x= " << x << endl;
+    cin.get();
 }
 
 
